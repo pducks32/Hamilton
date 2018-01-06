cf553cf887823464e1f7953ff35ef7bcb1737c6d
Patrick Metcalfe <git@patrickmetcalfe.com>
2018-01-06 01:36:41 -0600

    add quaternion tests (that I trust more)

--- Tests/HamiltonTests/HamiltonTests.swift
+++ Tests/HamiltonTests/HamiltonTests.swift
@@ -2,15 +2,4 @@ import XCTest
 @testable import Hamilton
 
 class HamiltonTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Hamilton().text, "Hello, World!")
-    }
-
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
--- Tests/HamiltonTests/QuaternionInitializers.swift
+++ Tests/HamiltonTests/QuaternionInitializers.swift
@@ -9,6 +9,11 @@
 import XCTest
 import Hamilton
 
+func XCTAssetCloseTo<X : FloatingPoint>(_ expected : X, actual : X, closerThan : X, file : StaticString = #file, line : UInt = #line) {
+    let distance = abs(actual - expected)
+    XCTAssertLessThan(distance, closerThan, file: file, line: line)
+}
+
 @available(iOS 10.0, OSX 10.12, *)
 class QuaternionInitializers: XCTestCase {
     
@@ -29,5 +34,12 @@ class QuaternionInitializers: XCTestCase {
         let actual = Quaternion(axis: Vector3.yAxis, angle: 6.patrians)
         let expected = Quaternion(w: 0.5.squareRoot(), x: 0, y: 0.5.squareRoot(), z: 0)
         AssertQuaternionComponentsEqual(expected, actual, within: Double.ulpOfOne)
+    }
+    
+    func testClosenessFromOnlineCalculator() {
+        let euler = EulerAngles(pitch: Degrees(91.860), yaw: Degrees(33.770), roll: Degrees(-111.272))
+        let quat = Quaternion(w: 0.548, x: 0.221, y: 0.682, z: -0.432)
+        let eulerAsQuat = Quaternion(eulerAngles: euler)
+        AssertQuaternionComponentsEqual(quat, eulerAsQuat, within: 0.0005)
     }
 }
