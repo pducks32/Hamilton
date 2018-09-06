9b740d8287584b9cc7ec279caea4544d205f024a
Patrick Metcalfe <git@patrickmetcalfe.com>
2018-09-06 00:49:56 -0500

    Fix scene converters on macOS

--- Sources/Hamilton/SceneKitExtensions/Vectorable+SceneKit.swift
+++ Sources/Hamilton/SceneKitExtensions/Vectorable+SceneKit.swift
@@ -10,13 +10,19 @@ import Foundation
 #if canImport(SceneKit)
 import SceneKit
 
+#if os(macOS)
+public typealias SCNComponent = CGFloat
+#else
+public typealias SCNComponent = Float
+#endif
+
 extension Vector3 {
     public init(scnVector: SCNVector3) {
         self = Vector3(x: Component(scnVector.x), y: Component(scnVector.y), z: Component(scnVector.z))
     }
     
     public var asSceneVector : SCNVector3 {
-        return SCNVector3(x: Float(x), y: Float(y), z: Float(z))
+        return SCNVector3(x: SCNComponent(x), y: SCNComponent(y), z: SCNComponent(z))
     }
 }
 
@@ -26,7 +32,7 @@ extension Quaternion {
     }
     
     public var asSceneQuaternion : SCNQuaternion {
-        return SCNQuaternion(x: Float(x), y: Float(y), z: Float(z), w: Float(w))
+        return SCNQuaternion(x: SCNComponent(x), y: SCNComponent(y), z: SCNComponent(z), w: SCNComponent(w))
     }
 }
 
